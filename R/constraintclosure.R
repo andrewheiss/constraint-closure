0aa89e8ce6aed6f5349a7e88fcd503ddccd5124b
Andrew Heiss <andrew@andrewheiss.com>
2019-03-19 16:23:22 -0600

    Add initial analysis

---
+++ R/constraintclosure.R
@@ -0,0 +1,8 @@
+#' The 'constraintclosure' package.
+#' 
+#' @description Research on ecological constraints and community dynamics
+#' 
+#' @docType package
+#' @name constraintclosure-package
+#' @aliases constraintclosure
+NULL
